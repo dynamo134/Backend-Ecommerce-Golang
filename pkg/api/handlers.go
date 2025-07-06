68281d1a923412fb914e4935e50c2014746dadd1
Dev-Shaharyar <shaharyar@instaview.ai>
2025-07-07 05:17:26 +0530

    fix: update handlers struct name for consistency and clarity

--- pkg/api/handlers.go
+++ pkg/api/handlers.go
@@ -1,13 +1,13 @@
 package handlers
 
-type handlers struct {
+type Handlers struct {
 	AuthHandler *AuthHandler
 }
 
 // NewHandlers creates a new handlers instance and initializes it with the given AuthHandler.
 // It returns the handlers instance.
-func NewHandlers(authHandler *AuthHandler) *handlers {
-	return &handlers{
+func NewHandlers(authHandler *AuthHandler) *Handlers {
+	return &Handlers{
 		AuthHandler: authHandler,
 	}
 }
