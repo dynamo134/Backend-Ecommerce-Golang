cf9a61f53e8ecbe82b498f14559594f71f408dcf
dynamo134 <shaharyaramu16@gmail.com>
2025-08-03 22:43:31 +0530

    feat: organize authentication and user routes in the server

--- cmd/server/auth_route.go
+++ cmd/server/auth_route.go
@@ -3,22 +3,8 @@ package server
 import "github.com/gin-gonic/gin"
 
 func setupAuthRoutes(r *gin.RouterGroup, h *HTTPServer) {
-	// Define user routes here
-	//r.GET("/", h.Handlers.GetAllUsers) // Example route to get all users
-	r.POST("/signup", h.Handlers.AuthHandler.SignUp) // Example route to create a new user
+	// Define auth routes here
+	r.POST("/signup", h.Handlers.AuthHandler.SignUp) 
 	r.POST("/signin", h.Handlers.AuthHandler.SignIn)
-	r.POST("/logout", h.Handlers.AuthHandler.Logout) // Example route to log out a user
-	// r.PUT("/:id", h.Handlers.UpdateUser) // Example route to update a user
-	// r.DELETE("/:id", h.Handlers.DeleteUser) // Example route to delete a user
-}
-
-
-func setupUserRoutes(r *gin.RouterGroup, h *HTTPServer) {
-	// Define user routes here
-	// r.GET("/", h.Handlers.GetAllUsers) // Example route to get all users
-	// r.POST("/signup", h.Handlers.AuthHandler.SignUp) // Example route to create a new user
-	// r.POST("/signin", h.Handlers.AuthHandler.SignIn)
-	// r.POST("/logout", h.Handlers.AuthHandler.Logout) // Example route to log out a user
-	// r.PUT("/:id", h.Handlers.UpdateUser) // Example route to update a user
-	// r.DELETE("/:id", h.Handlers.DeleteUser) // Example route to delete a user
+	r.POST("/logout", h.Handlers.AuthHandler.Logout)
 }
---
+++ cmd/server/user_route.go
@@ -0,0 +1,11 @@
+package server
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+
+func setupUserRoutes(r *gin.RouterGroup, h *HTTPServer) {
+	// Define user routes here
+
+}
